6ec68564ad20cabf2357506b367b96f3dd19ab4f
Linus Oleander <linus@oleander.nu>
2017-04-05 11:12:33 +0200

    Update

--- Package.swift
+++ Package.swift
@@ -8,8 +8,8 @@ let package = Package(
       .Package(url: "https://github.com/JohnSundell/Files.git", majorVersion: 1),
       .Package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", majorVersion: 3),
       .Package(url: "https://github.com/oleander/SwiftyTimer.git", majorVersion: 2),
-      .Package(url: "https://github.com/oleander/FootlessParser.git", majorVersion: 2)
-      // .Package(url: "https://github.com/Quick/Quick.git", majorVersion: 1),
-      // .Package(url: "https://github.com/Quick/Nimble.git", majorVersion: 6),
+      .Package(url: "https://github.com/oleander/FootlessParser.git", majorVersion: 2),
+      .Package(url: "https://github.com/Quick/Quick.git", majorVersion: 1),
+      .Package(url: "https://github.com/Quick/Nimble.git", majorVersion: 6)
     ]
 )
